1cbd3b783018d5bfcc32520ba2e07eeddcba777f
Vladislav Gerasimov <projectmediastudio306@gmail.com>
2026-02-28 18:56:57 +0300

    Финальное исправление комментариев

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views.generic import (
     ListView, DetailView, CreateView,
@@ -6,9 +6,10 @@ from django.views.generic import (
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.db.models import Count
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.conf import settings
 from .models import Post, Comment, Category
 from .forms import PostForm, CommentForm, ProfileEditForm
@@ -128,31 +129,18 @@ class PostDeleteView(AuthorRequiredMixin, DeleteView):
         return reverse('blog:profile', args=[self.request.user.username])
 
 
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'blog/comment.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post_id = self.kwargs['post_id']
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse('blog:post_detail', args=[self.kwargs['post_id']])
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['post'] = get_object_or_404(Post, id=self.kwargs['post_id'])
-        return context
-
-    def post(self, request, *args, **kwargs):
-        self.object = None
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        if text:
+            Comment.objects.create(
+                text=text,
+                author=request.user,
+                post=post
+            )
+    return redirect('blog:post_detail', post_id=post_id)
 
 
 class CommentUpdateView(CommentAuthorRequiredMixin, UpdateView):
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,22 +1,58 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('auth/registration/', include('registration.urls')),
-    path('', include('blog.urls')),
-    path('pages/', include('pages.urls')),
+    path('', views.PostListView.as_view(), name='index'),
+    path(
+        'profile/edit/',
+        views.ProfileEditView.as_view(),
+        name='edit_profile'
+    ),
+    path(
+        'profile/<str:username>/',
+        views.ProfileView.as_view(),
+        name='profile'
+    ),
+    path(
+        'posts/create/',
+        views.PostCreateView.as_view(),
+        name='create_post'
+    ),
+    path(
+        'posts/<int:post_id>/',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
+    path(
+        'posts/<int:post_id>/edit/',
+        views.PostUpdateView.as_view(),
+        name='edit_post'
+    ),
+    path(
+        'posts/<int:post_id>/delete/',
+        views.PostDeleteView.as_view(),
+        name='delete_post'
+    ),
+    path(
+        'posts/<int:post_id>/comment/',
+        views.add_comment,
+        name='add_comment'
+    ),
+    path(
+        'posts/<int:post_id>/edit_comment/<int:comment_id>/',
+        views.CommentUpdateView.as_view(),
+        name='edit_comment'
+    ),
+    path(
+        'posts/<int:post_id>/delete_comment/<int:comment_id>/',
+        views.CommentDeleteView.as_view(),
+        name='delete_comment'
+    ),
+    path(
+        'category/<slug:category_slug>/',
+        views.CategoryListView.as_view(),
+        name='category_posts'
+    ),
 ]
-
-handler403 = 'pages.views.csrf_failure'
-handler404 = 'pages.views.page_not_found'
-handler500 = 'pages.views.server_error'
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
